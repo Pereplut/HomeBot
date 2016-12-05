39eead46c284b9ae63b22ba0434c992cc0c6340b
Pereplut <d.guyvan@gmail.com>
2016-12-05 23:56:32 +0100

    house bot

--- ButlerBot.py
+++ ButlerBot.py
@@ -10,14 +10,15 @@ logger.debug('launching ButlerBot')
 
 bot = telebot.TeleBot(token)
 
+
 @bot.message_handler(commands=['help', 'start'])
 def send_welcome(message):
     bot.reply_to(message, """Hi there """)
 
 @bot.message_handler(commands=["temperature"])
 def send_temperature(message):
-    with open('/home/dave/PYrojects/Temperature_log/cpu_current_temp.csv','r') as temperature_file:
-        responce=temperature_file.read()
+    with open('/home/dave/PYrojects/Temperature_log/cpu_current_temp.csv', 'r') as temperature_file:
+        responce = temperature_file.read()
     bot.send_message(message.chat.id, responce)
 
 if __name__ == '__main__':
